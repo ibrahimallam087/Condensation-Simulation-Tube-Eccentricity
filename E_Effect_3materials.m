b11d877de7a834962b1b1c4c056b6408d0728534
ibrahim thabet allam <68043509+ibrahimallam087@users.noreply.github.com>
2025-01-11 13:09:35 +0200

    Update E_Effect_3materials.m

--- E_Effect_3materials.m
+++ E_Effect_3materials.m
@@ -2,14 +2,13 @@ clc
 clear 
 close all
 
-% Define data for eccentricity and wall average heat flux Coefficinet
-
+% Define data for eccentricity and wall average heat flux coefficient
 eccentricity = [0.2, 0.4, 0.6, 0.8];
 
 % Wall average heat flux values (example data, replace with your values)
-heat_flux_R32a = -[     -63409.9,     -64326.8,  -64812.6, -64812.6]/1000/50; %Kw/K
-heat_flux_R134a =- [  -49402,   -50138.3, -50656.5 ,   -50677.6]/1000/50;%Kw/K
-heat_flux_R410a = -[    -57302.4,    -58375.3,   -58844.9 ,   -59011 ]/1000/50;%Kw/K
+heat_flux_R32a = -[      -63409.9,     -64326.8,  -64812.6, -64812.6]/1000/50; % Kw/K
+heat_flux_R134a = -[  -49402,   -50138.3, -50656.5 ,   -50677.6]/1000/50; % Kw/K
+heat_flux_R410a = -[    -57302.4,    -58375.3,   -58844.9 ,   -59011 ]/1000/50; % Kw/K
 
 % Create the plot
 figure;
@@ -20,8 +19,9 @@ plot(eccentricity, heat_flux_R410a, '-^', 'LineWidth', 1.5, 'MarkerSize', 8, 'Co
 hold off;
 
 % Add labels, legend, and title
-xlabel('$Eccentricity$', 'Interpreter', 'latex', 'FontSize', 14);
-ylabel('$ Heat Flux Coefficient  kW/m^{2}/K$', 'Interpreter','latex', 'FontSize',14);
+xlabel('Eccentricity', 'Interpreter', 'latex', 'FontSize', 14);
+ylabel('$\bar{h}$ (kW/m$^2$/K)', 'Interpreter', 'latex', 'FontSize', 14);
+
 title('$Wall\ Average\ Heat\ Flux\ for\ Different\ Refrigerants$', 'Interpreter', 'latex', 'FontSize', 16);
 legend('Location', 'best', 'Interpreter', 'latex', 'FontSize', 12);
 grid on;
